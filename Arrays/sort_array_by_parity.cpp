960914d1c23b9d9d2a4351f61f9ad8c4102c689b
Johan Ferreras <ferrerasjohan@gmail.com>
2019-06-22 22:55:02 -0400

    Solution 1

--- Arrays/sort_array_by_parity.cpp
+++ Arrays/sort_array_by_parity.cpp
@@ -14,3 +14,15 @@ public:
         return retArr;
     }
 };
+
+/* NOTES
+
+I thought of adding the vector of odd numbers to the vector to return using a for loop
+but after reading the cpp documentation I remembered the insert function uses iterators.
+
+Runtime: 28 ms, faster than 86.92% of C++ online submissions for Sort Array By Parity.
+Memory Usage: 10 MB, less than 37.68% of C++ online submissions for Sort Array By Parity.
+
+I would like to learn to make my code faster and lighter.
+
+*/
